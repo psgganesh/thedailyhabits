c1c68373b421211d2ba79cd7e639472ba181aa38
Shankar <psgganesh@gmail.com>
2019-07-22 23:21:04 +0800

    Method 1 - Layout as a Higher Order Component

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -2,14 +2,17 @@ import Header from "./Header";
 
 const layoutStyle ={
   margin: 20,
-  padding: 20
+  padding: 20,
+  border: '1px solid #DDD'
 };
 
-const Layout = props => (
-  <div style={layoutStyle}>
-    <Header />
-    {props.children}
-  </div>
-);
+const withLayout = Page => {
+  return () => (
+    <div style={layoutStyle}>
+      <Header />
+      <Page />
+    </div>
+  );
+};
 
-export default Layout;
+export default withLayout;
