3c19bd2bb124e585e0c982ab9a418c81504771aa
Shankar <psgganesh@gmail.com>
2019-08-09 00:45:49 +0800

    Only change in tasks vue

--- components/panels/Tasks.vue
+++ components/panels/Tasks.vue
@@ -2,24 +2,23 @@
   <div>
     <div class="atomic-canvas-list planboard-list">
       <h2 class="text-white text-center"><a-icon type="calendar" /> Today</h2>
-        
-        <draggable :list="tasks"
+        <draggable v-model="habits"
           group="atomichabits"
-          sort="false"
+          sort="true"
           class="pane"
           draggable=".item"
           animation="150"
           easing="cubic-bezier(1, 0, 0, 1)"
           ghostClass="ghost"
           dragClass="sortable-drag"
+          @add="onAdd"
         >
-        <div v-for="element in tasks" :key="element.name">
-
+        <!-- {{ habits }} -->
+        <!-- <a-card :title="element.task" class="list-group-item item" :bordered="true" v-for="element in habits.todoListData" :key="element.task" /> -->
+        <div v-for="element in habits" :key="element.name" class="list-group">
           <a-card :title="element.task" class="list-group-item item" :bordered="true" v-for="element in element.todoListData" :key="element.task" />
-
         </div>
       </draggable>
-
     </div>
   </div>
 </template>
@@ -30,16 +29,26 @@ export default {
   layout: 'simple',
   data() {
     return {
-      tasks: []
+      habits: []
     };
   },
   methods: {
-    
+    onAdd (evt) {
+      console.log(evt);
+    },
   }
 };
 </script>
 
 <style>
+.list-group {
+  border-top: 0px;
+  border-right: 0px;
+  border-bottom: 1px;
+  border-left: 0px;
+  border-style: dashed;
+  border-color: #ffffff;
+}
 .todo {
   margin: 10px 0px;
 }
