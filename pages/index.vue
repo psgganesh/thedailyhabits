b57fe7693b8a4d8d48d64806b4aa6640f1a5c34e
Shankar <psgganesh@gmail.com>
2019-08-27 22:42:53 +0800

    Adding features list - WIP

--- pages/index.vue
+++ pages/index.vue
@@ -94,7 +94,7 @@
                                   </div>
                                   <div class="services-content media-body">
                                       <h4 class="services-title">Startup</h4>
-                                      <p class="text">Short description for the ones</br> who look for something new.</p>
+                                      <p class="text">Short description for the <br/> who look for something new.</p>
                                   </div>
                               </div> <!-- services content -->
                           </div>
@@ -105,7 +105,7 @@
                                   </div>
                                   <div class="services-content media-body">
                                       <h4 class="services-title">SaaS Business</h4>
-                                      <p class="text">Short description for the ones</br> who look for something new.</p>
+                                      <p class="text">Short description for the <br/> who look for something new.</p>
                                   </div>
                               </div> <!-- services content -->
                           </div>
@@ -116,7 +116,7 @@
                                   </div>
                                   <div class="services-content media-body">
                                       <h4 class="services-title">Agency</h4>
-                                      <p class="text">Short description for the ones</br> who look for something new.</p>
+                                      <p class="text">Short description for the <br/> who look for something new.</p>
                                   </div>
                               </div> <!-- services content -->
                           </div>
@@ -127,7 +127,7 @@
                                   </div>
                                   <div class="services-content media-body">
                                       <h4 class="services-title">App Landing</h4>
-                                      <p class="text">Short description for the ones</br> who look for something new.</p>
+                                      <p class="text">Short description for the <br/> who look for something new.</p>
                                   </div>
                               </div> <!-- services content -->
                           </div>
@@ -146,93 +146,122 @@
 
       <!--====== PRICING PART START ======-->
 
-      <section id="pricing" class="pricing-area">
+      <section id="pricing" class="pricing-area"> 
           <div class="container">
-              <div class="row justify-content-center">
-                  <div class="col-lg-12">
-                      <div class="section-title text-right pb-10">
-                          <h4 class="title">Our Pricing</h4>
-                          <p class="text">Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed!</p>
-                      </div> <!-- section title -->
+              <div class="row">
+                <div class="col-lg-4">
+                  <div class="image">
+                    <img src="images/services.png" alt="Services">
                   </div>
+                </div>
+                  <div class="col-lg-8">
+                      <div class="row">
+                          <div class="col-md-6">
+                              <div class="services-content mt-40 d-sm-flex">
+                                  <div class="services-icon">
+                                      <i class="lni-bolt"></i>
+                                  </div>
+                                  <div class="services-content media-body">
+                                      <h4 class="services-title">Startup</h4>
+                                      <p class="text">Short description for the <br/> who look for something new.</p>
+                                  </div>
+                              </div> <!-- services content -->
+                          </div>
+                          <div class="col-md-6">
+                              <div class="services-content mt-40 d-sm-flex">
+                                  <div class="services-icon">
+                                      <i class="lni-bar-chart"></i>
+                                  </div>
+                                  <div class="services-content media-body">
+                                      <h4 class="services-title">SaaS Business</h4>
+                                      <p class="text">Short description for the <br/> who look for something new.</p>
+                                  </div>
+                              </div> <!-- services content -->
+                          </div>
+                          <div class="col-md-6">
+                              <div class="services-content mt-40 d-sm-flex">
+                                  <div class="services-icon">
+                                      <i class="lni-brush"></i>
+                                  </div>
+                                  <div class="services-content media-body">
+                                      <h4 class="services-title">Agency</h4>
+                                      <p class="text">Short description for the <br/> who look for something new.</p>
+                                  </div>
+                              </div> <!-- services content -->
+                          </div>
+                          <div class="col-md-6">
+                              <div class="services-content mt-40 d-sm-flex">
+                                  <div class="services-icon">
+                                      <i class="lni-bulb"></i>
+                                  </div>
+                                  <div class="services-content media-body">
+                                      <h4 class="services-title">App Landing</h4>
+                                      <p class="text">Short description for the <br/> who look for something new.</p>
+                                  </div>
+                              </div> <!-- services content -->
+                          </div>
+                      </div> <!-- row -->
+                  </div> <!-- row -->
               </div> <!-- row -->
-              <div class="row justify-content-center">
-                  <div class="col-lg-4 col-md-7 col-sm-9">
-                      <div class="single-pricing mt-40">
-                          <div class="pricing-header text-center">
-                              <h5 class="sub-title">Basic</h5>
-                              <span class="price">$ 199</span>
-                              <p class="year">per year</p>
+          </div> <!-- conteiner -->
+      </section>
+
+
+      <section id="pricing" class="pricing-area bg-white"> 
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-8">
+                      <div class="row">
+                          <div class="col-md-6">
+                              <div class="services-content mt-40 d-sm-flex">
+                                  <div class="services-icon">
+                                      <i class="lni-bolt"></i>
+                                  </div>
+                                  <div class="services-content media-body">
+                                      <h4 class="services-title">Startup</h4>
+                                      <p class="text">Short description for the <br/> who look for something new.</p>
+                                  </div>
+                              </div> <!-- services content -->
                           </div>
-                          <div class="pricing-list">
-                              <ul>
-                                  <li><i class="lni-check-mark-circle"></i> Carefully crafted components</li>
-                                  <li><i class="lni-check-mark-circle"></i> Amazing page examples</li>
-                                  <li><i class="lni-check-mark-circle"></i> Super friendly support team</li>
-                                  <li><i class="lni-check-mark-circle"></i> Awesome Support</li>
-                              </ul>
+                          <div class="col-md-6">
+                              <div class="services-content mt-40 d-sm-flex">
+                                  <div class="services-icon">
+                                      <i class="lni-bar-chart"></i>
+                                  </div>
+                                  <div class="services-content media-body">
+                                      <h4 class="services-title">SaaS Business</h4>
+                                      <p class="text">Short description for the <br/> who look for something new.</p>
+                                  </div>
+                              </div> <!-- services content -->
                           </div>
-                          <div class="pricing-btn text-center">
-                              <a class="main-btn" href="#">GET STARTED</a>
+                          <div class="col-md-6">
+                              <div class="services-content mt-40 d-sm-flex">
+                                  <div class="services-icon">
+                                      <i class="lni-brush"></i>
+                                  </div>
+                                  <div class="services-content media-body">
+                                      <h4 class="services-title">Agency</h4>
+                                      <p class="text">Short description for the <br/> who look for something new.</p>
+                                  </div>
+                              </div> <!-- services content -->
                           </div>
-                          <div class="buttom-shape">
-                              <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 112.35"><defs><style>.color-1{fill:#2bdbdc;isolation:isolate;}.cls-1{opacity:0.1;}.cls-2{opacity:0.2;}.cls-3{opacity:0.4;}.cls-4{opacity:0.6;}</style></defs><title>bottom-part1</title><g id="bottom-part"><g id="Group_747" data-name="Group 747"><path id="Path_294" data-name="Path 294" class="cls-1 color-1" d="M0,24.21c120-55.74,214.32,2.57,267,0S349.18,7.4,349.18,7.4V82.35H0Z" transform="translate(0 0)"/><path id="Path_297" data-name="Path 297" class="cls-2 color-1" d="M350,34.21c-120-55.74-214.32,2.57-267,0S.82,17.4.82,17.4V92.35H350Z" transform="translate(0 0)"/><path id="Path_296" data-name="Path 296" class="cls-3 color-1" d="M0,44.21c120-55.74,214.32,2.57,267,0S349.18,27.4,349.18,27.4v74.95H0Z" transform="translate(0 0)"/><path id="Path_295" data-name="Path 295" class="cls-4 color-1" d="M349.17,54.21c-120-55.74-214.32,2.57-267,0S0,37.4,0,37.4v74.95H349.17Z" transform="translate(0 0)"/></g></g></svg>
+                          <div class="col-md-6">
+                              <div class="services-content mt-40 d-sm-flex">
+                                  <div class="services-icon">
+                                      <i class="lni-bulb"></i>
+                                  </div>
+                                  <div class="services-content media-body">
+                                      <h4 class="services-title">App Landing</h4>
+                                      <p class="text">Short description for the <br/> who look for something new.</p>
+                                  </div>
+                              </div> <!-- services content -->
                           </div>
-                      </div> <!-- single pricing -->
-                  </div>
-                  
-                  <div class="col-lg-4 col-md-7 col-sm-9">
-                      <div class="single-pricing pro mt-40">
-                          <div class="pricing-baloon">
-                              <img src="images/baloon.svg" alt="baloon">
-                          </div>
-                          <div class="pricing-header">
-                              <h5 class="sub-title">Pro</h5>
-                              <span class="price">$ 399</span>
-                              <p class="year">per year</p>
-                          </div>
-                          <div class="pricing-list">
-                              <ul>
-                                  <li><i class="lni-check-mark-circle"></i> Carefully crafted components</li>
-                                  <li><i class="lni-check-mark-circle"></i> Amazing page examples</li>
-                                  <li><i class="lni-check-mark-circle"></i> Super friendly support team</li>
-                                  <li><i class="lni-check-mark-circle"></i> Awesome Support</li>
-                              </ul>
-                          </div>
-                          <div class="pricing-btn text-center">
-                              <a class="main-btn" href="#">GET STARTED</a>
-                          </div>
-                          <div class="buttom-shape">
-                              <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 112.35"><defs><style>.color-2{fill:#0067f4;isolation:isolate;}.cls-1{opacity:0.1;}.cls-2{opacity:0.2;}.cls-3{opacity:0.4;}.cls-4{opacity:0.6;}</style></defs><title>bottom-part1</title><g id="bottom-part"><g id="Group_747" data-name="Group 747"><path id="Path_294" data-name="Path 294" class="cls-1 color-2" d="M0,24.21c120-55.74,214.32,2.57,267,0S349.18,7.4,349.18,7.4V82.35H0Z" transform="translate(0 0)"/><path id="Path_297" data-name="Path 297" class="cls-2 color-2" d="M350,34.21c-120-55.74-214.32,2.57-267,0S.82,17.4.82,17.4V92.35H350Z" transform="translate(0 0)"/><path id="Path_296" data-name="Path 296" class="cls-3 color-2" d="M0,44.21c120-55.74,214.32,2.57,267,0S349.18,27.4,349.18,27.4v74.95H0Z" transform="translate(0 0)"/><path id="Path_295" data-name="Path 295" class="cls-4 color-2" d="M349.17,54.21c-120-55.74-214.32,2.57-267,0S0,37.4,0,37.4v74.95H349.17Z" transform="translate(0 0)"/></g></g></svg>
-                          </div>
-                      </div> <!-- single pricing -->
-                  </div>
-                  
-                  <div class="col-lg-4 col-md-7 col-sm-9">
-                      <div class="single-pricing enterprise mt-40">
-                          <div class="pricing-flower">
-                              <img src="images/flower.svg" alt="flower">
-                          </div>
-                          <div class="pricing-header text-right">
-                              <h5 class="sub-title">Enterprise</h5>
-                              <span class="price">$ 799</span>
-                              <p class="year">per year</p>
-                          </div>
-                          <div class="pricing-list">
-                              <ul>
-                                  <li><i class="lni-check-mark-circle"></i> Carefully crafted components</li>
-                                  <li><i class="lni-check-mark-circle"></i> Amazing page examples</li>
-                                  <li><i class="lni-check-mark-circle"></i> Super friendly support team</li>
-                                  <li><i class="lni-check-mark-circle"></i> Awesome Support</li>
-                              </ul>
-                          </div>
-                          <div class="pricing-btn text-center">
-                              <a class="main-btn" href="#">GET STARTED</a>
-                          </div>
-                          <div class="buttom-shape">
-                              <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 112.35"><defs><style>.color-3{fill:#4da422;isolation:isolate;}.cls-1{opacity:0.1;}.cls-2{opacity:0.2;}.cls-3{opacity:0.4;}.cls-4{opacity:0.6;}</style></defs><title>bottom-part1</title><g id="bottom-part"><g id="Group_747" data-name="Group 747"><path id="Path_294" data-name="Path 294" class="cls-1 color-3" d="M0,24.21c120-55.74,214.32,2.57,267,0S349.18,7.4,349.18,7.4V82.35H0Z" transform="translate(0 0)"/><path id="Path_297" data-name="Path 297" class="cls-2 color-3" d="M350,34.21c-120-55.74-214.32,2.57-267,0S.82,17.4.82,17.4V92.35H350Z" transform="translate(0 0)"/><path id="Path_296" data-name="Path 296" class="cls-3 color-3" d="M0,44.21c120-55.74,214.32,2.57,267,0S349.18,27.4,349.18,27.4v74.95H0Z" transform="translate(0 0)"/><path id="Path_295" data-name="Path 295" class="cls-4 color-3" d="M349.17,54.21c-120-55.74-214.32,2.57-267,0S0,37.4,0,37.4v74.95H349.17Z" transform="translate(0 0)"/></g></g></svg>
-                          </div>
-                      </div> <!-- single pricing -->
+                      </div> <!-- row -->
+                  </div> <!-- row -->
+                  <div class="col-lg-4">
+                    <div class="image">
+                      <img src="images/services.png" alt="Services">
+                    </div>
                   </div>
               </div> <!-- row -->
           </div> <!-- conteiner -->
@@ -442,6 +471,14 @@ export default {
 </script>
 
 <style>
+.header-hero .header-content .text {
+  color: #0067f4 !important;
+}
+.header-hero::before {
+background: -webkit-linear-gradient(#e8edffe6, #effeffe6 50%, #ffffff1f, #ffffff1f);
+background: -o-linear-gradient(#e8edffe6, #effeffe6 50%, #ffffff1f, #ffffff1f);
+background: linear-gradient(#e8edffe6, #effeffe6 50%, #ffffff1f, #ffffff1f) !important;
+}
 .header-hero .header-content .header-btn li a.main-btn.btn-one::before {
   background: url('/images/blockstack.png') left no-repeat !important;
   background-size: contain !important;
