620faba7fe428b73d3df6fc5379ca2811f7900a8
Shankar <psgganesh@gmail.com>
2019-10-09 20:36:35 +0800

    navbar avatar

--- components/NavigationDrawer.vue
+++ components/NavigationDrawer.vue
@@ -11,11 +11,11 @@
     <template v-slot:prepend>
       <v-list-item two-line>
         <v-list-item-avatar>
-          <img src="https://randomuser.me/api/portraits/women/81.jpg" />
+          <img :src="user.avatarUrl()" />
         </v-list-item-avatar>
 
         <v-list-item-content>
-          <v-list-item-title>Jane Smith</v-list-item-title>
+          <v-list-item-title>{{ username }}</v-list-item-title>
           <v-list-item-subtitle>Logged In</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
@@ -81,6 +81,8 @@ export default {
   name: "NavigationDrawer",
   data() {
     return {
+      user: null,
+      username: null,
       options: [{ icon: "mdi-logout", text: "Logout" }]
     };
   },
