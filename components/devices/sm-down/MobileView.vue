b4053abde945a53e2d63d9b5023e48ae80300302
Shankar <psgganesh@gmail.com>
2019-10-11 15:36:40 +0800

    warning message

--- components/devices/sm-down/MobileView.vue
+++ components/devices/sm-down/MobileView.vue
@@ -1,8 +1,17 @@
 <template>
   <v-row no-gutters>
-    <v-col cols="1" class="px-15"></v-col>
+    <v-col cols="11" class="px-15">
+      <v-alert
+        border="bottom"
+        colored-border
+        type="warning"
+        class="yellow"
+        elevation="2"
+      >This application is best optimized only for desktop now. Future versions will be having a mobile responsive view and also a mobile app soon.</v-alert>
+    </v-col>
+    <!-- <v-col cols="1" class="px-15"></v-col>
     <v-col cols="8" class="px-15"></v-col>
-    <v-col cols="3" class="px-15"></v-col>
+    <v-col cols="3" class="px-15"></v-col>-->
   </v-row>
 </template>
 
