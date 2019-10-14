ef2ea14660b0e1e620775e9d0715ea7c17f7d074
Shankar <psgganesh@gmail.com>
2019-10-14 15:50:35 +0800

    Mobile view UI and CSS

--- components/devices/sm-down/MobileView.vue
+++ components/devices/sm-down/MobileView.vue
@@ -1,22 +1,60 @@
 <template>
-  <v-row no-gutters>
-    <v-col cols="11" class="px-15">
-      <v-alert
-        border="bottom"
-        colored-border
-        type="warning"
-        class="yellow"
-        elevation="2"
-      >This application is best optimized only for desktop now. Future versions will be having a mobile responsive view and also a mobile app soon.</v-alert>
-    </v-col>
-    <!-- <v-col cols="1" class="px-15"></v-col>
-    <v-col cols="8" class="px-15"></v-col>
-    <v-col cols="3" class="px-15"></v-col>-->
-  </v-row>
+  <div>
+    <template v-for="(item, index) in habits">
+      <v-list-item :key="index" :class="computedCardClass(item)">
+        <v-list-item-icon>
+          <v-icon>mdi-drag</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.activity"></v-list-item-title>
+          <v-list-item-subtitle class="text--primary" v-text="item.title"></v-list-item-subtitle>
+          <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
+        </v-list-item-content>
+
+        <v-list-item-action>
+          <v-list-item-action-text v-text="computedDays(item)"></v-list-item-action-text>
+          <!-- <v-icon v-if="!active" color="grey lighten-1">star_border</v-icon>
+          <v-icon v-else color="yellow">star</v-icon>-->
+        </v-list-item-action>
+      </v-list-item>
+      <v-divider v-if="index + 1 < habits.length" :key="index"></v-divider>
+    </template>
+    <BottomNavigation />
+  </div>
 </template>
 
 <script>
+import moment from "moment";
+import Panels from "~/components/devices/md-up/Panels";
+import BottomNavigation from "~/components/devices/sm-down/BottomNavigation";
+
 export default {
-  name: "MobileView"
+  name: "MobileView",
+  components: {
+    Panels,
+    BottomNavigation
+  },
+  computed: {
+    habits: {
+      get() {
+        return this.$store.state.habits;
+      },
+      set(value) {
+        const data = {
+          zone: "habits",
+          habit: value
+        };
+        this.$store.dispatch("moveHabit", data);
+      }
+    }
+  },
+  methods: {
+    computedDays(item) {
+      return moment(item.endsOn).diff(item.startsFrom, "days") + " days";
+    },
+    computedCardClass(item) {
+      return "card-border-color card-border-color-" + item.category;
+    }
+  }
 };
 </script>
