b47b89a1d8682cceca0a3e2d729eb90a68415015
Shankar <psgganesh@gmail.com>
2020-02-27 00:51:29 +0800

    Restore index page

--- layouts/simple.vue
+++ layouts/simple.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-block">
+  <div id="preloader">
     <nuxt />
   </div>
 </template>
@@ -10,11 +10,65 @@ export default {
   head: {
     titleTemplate: "thedailyhabits - %s",
     title: "A habits journal centred around privacy",
-    link: [
-      { rel: "stylesheet", href: "frontend/css/base.css" },
-      { rel: "stylesheet", href: "frontend/css/vendor.css" },
-      { rel: "stylesheet", href: "frontend/css/main.css" }
-    ]
+    link: [{ rel: "stylesheet", href: "/frontend.css" }]
   }
 };
 </script>
+
+<style>
+.d-none {
+  display: none;
+}
+.d-block {
+  display: block;
+}
+#main {
+  font-family: Merriweather Sans, sans-serif !important;
+}
+.header-hero .header-content .text {
+  color: #0067f4 !important;
+}
+.header-hero::before {
+  background: -webkit-linear-gradient(
+    #e8edffe6,
+    #effeffe6 20%,
+    #ffffff1f,
+    #ffffff1f
+  );
+  background: -o-linear-gradient(
+    #e8edffe6,
+    #effeffe6 20%,
+    #ffffff1f,
+    #ffffff1f
+  );
+  background: linear-gradient(
+    #e8edffe6,
+    #effeffe6 20%,
+    #ffffff1f,
+    #ffffff1f
+  ) !important;
+}
+.header-hero .header-content .header-btn li a.main-btn.btn-one::before {
+  background: url("/images/blockstack.png") left no-repeat !important;
+  background-size: contain !important;
+}
+.header-hero .header-content .header-btn li a.main-btn {
+  width: -webkit-fill-available !important;
+  padding-left: 55px !important;
+  background-color: #1f1f6c !important;
+}
+.btn-one {
+  border: 5px solid !important;
+}
+.padded {
+  padding-top: 120px;
+  padding-bottom: 120px;
+}
+.bg-light {
+  background-color: #f4f6f7;
+}
+.section-title .title {
+  font-size: 50px !important;
+  font-family: Merriweather Sans, sans-serif !important;
+}
+</style>
